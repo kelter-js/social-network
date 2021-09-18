4edbca511d9c73717e8feafa8c88e8ebb0096a16
kelter-js <spawnkillerqw@gmail.com>
2021-09-18 13:03:13 +0400

    Добавляет стили и базовую разметку для подвала и шапки

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { PureComponent } from 'react'
+import { Header } from './header.js'
+import { Body } from './body.js'
+import { Footer } from './footer.js'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Body />
+      <Footer />
+    </>
   );
 }
 
-export default App;
+export { App }
---
+++ src/header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className = 'page-header container'>
+    </header>
+  );
+}
+
+export { Header }
