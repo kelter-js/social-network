674c577de71bc63bd18ce3b4a12246cfe344e9b5
kelter-js <spawnkillerqw@gmail.com>
2021-09-22 19:05:23 +0400

    Добавляет разбитие мейн контента в отдельный класс, рефакторинг компонента main

--- src/components/main/index.js
+++ src/components/main/index.js
@@ -1,77 +1,23 @@
 import React, { PureComponent } from 'react'
-import { Profile } from './profile.js'
 import { Navigation } from './navigation-list.js'
-import { FeedList } from './feed-list.js'
+import { MainPageContent } from './main-content.js'
 
-class Main extends PureComponent {
-  #defaultMessage;
-
-  constructor (props) {
-    super(props);
-    this.#defaultMessage = '...start your message here.';
-
-    this.state = {
-      'message': this.#defaultMessage,
-    };
-
-    this.onChange = this.onChange.bind(this);
-    this.onFocus = this.onFocus.bind(this);
-    this.onBlur = this.onBlur.bind(this);
-  }
-
-  onChange (e) {
-    this.setState({
-      'message': e.target.value,
-    });
-  }
-
-  onFocus () {
-    if (this.state.message !== this.#defaultMessage) {
-      return;
-    }
-
-    this.setState({
-      'message': '',
-    });
-  }
-
-  onBlur () {
-    if (this.state.message) {
-      return;
-    }
-
-    this.setState({
-      'message': this.#defaultMessage,
-    });
-  }
-
-  render () {
-    return (
-      <>
-        <main className = 'page-main container'>
-          <h1 className = 'visually-hidden'>Социальная сеть ВРеакте</h1>
-          <nav className = 'page-main__navigation'>
-            <Navigation navigation = {this.props.navigation}/>
-            <a className = 'navigation__link' href = '#'>Settings</a>
-          </nav>
-          <section className = 'page-main__profile-wrapper'>
-            <h2 className = 'visually-hidden'>Профиль пользователя</h2>
-            <Profile data = {this.props.user}/>
-            <form className = 'page-main__news'>
-              <label className = 'news__label'>
-                Share news with your friends!
-                <textarea className = 'news__message' value = { this.state.message } id = 'newsMessage' onChange = { this.onChange } onBlur = { this.onBlur } onFocus = { this.onFocus }/>
-              </label>
-              <input className = 'news__submit' type="submit" value="Отправить" />
-            </form>
-            <div className = 'page-feed'>
-              <FeedList feed = { this.props.feed }/>
-            </div>
-          </section>
-        </main>
-      </>
-    );
-  }
+const Main = (props) => {
+  return (
+    <main className='page-main container'>
+      <h1 className='visually-hidden'>Социальная сеть ВРеакте</h1>
+      <nav className='page-main__navigation'>
+        <Navigation navigation={props.navigation} />
+        <a className='navigation__link' href='#'>Settings</a>
+      </nav>
+      <section className='page-main__content-wrapper'>
+        <h2 className='visually-hidden'>Профиль пользователя</h2>
+        <div>
+          <MainPageContent data={props} />
+        </div>
+      </section>
+    </main>
+  );
 }
 
 export { Main }
---
+++ src/components/main/main-content.js
@@ -0,0 +1,66 @@
+import React, { PureComponent } from 'react'
+import { Profile } from './profile.js'
+import { FeedList } from './feed-list.js'
+
+class MainPageContent extends PureComponent {
+  #defaultMessage;
+
+  constructor(props) {
+    super(props);
+    this.#defaultMessage = '...start your message here.';
+
+    this.state = {
+      'message': this.#defaultMessage,
+    };
+
+    this.onChange = this.onChange.bind(this);
+    this.onFocus = this.onFocus.bind(this);
+    this.onBlur = this.onBlur.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({
+      'message': e.target.value,
+    });
+  }
+
+  onFocus() {
+    if (this.state.message !== this.#defaultMessage) {
+      return;
+    }
+
+    this.setState({
+      'message': '',
+    });
+  }
+
+  onBlur() {
+    if (this.state.message) {
+      return;
+    }
+
+    this.setState({
+      'message': this.#defaultMessage,
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <Profile data={this.props.data.user} />
+        <form className='page-main__news'>
+          <label className='news__label'>
+            Share news with your friends!
+            <textarea className='news__message' value={this.state.message} id='newsMessage' onChange={this.onChange} onBlur={this.onBlur} onFocus={this.onFocus} />
+          </label>
+          <input className='news__submit' type="submit" value="Отправить" />
+        </form>
+        <div className='page-feed'>
+          <FeedList feed={this.props.data.feed} />
+        </div>
+      </>
+    );
+  }
+}
+
+export { MainPageContent }
